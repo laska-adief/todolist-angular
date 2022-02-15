654e7457c05c5388da53c6bfe822314a4d83cc7b
laska-adief <laskaadief.la@gmail.com>
2022-02-15 19:36:59 +0700

    implement function get todolist in todo-list component

--- src/app/todos/todo-list/todo-list.component.ts
+++ src/app/todos/todo-list/todo-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent implements OnInit {
-
-  constructor() { }
+  todolist: any = [];
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.getTodoList();
   }
 
+  getTodoList() {
+    this.todoService.getTodos().subscribe((resp) => {
+      const todolistData = resp.filter((todo: any) => todo.isompleted !== true);
+      this.todolist = todolistData;
+    });
+  }
 }
