70d4b39cfc58cf31813e061140ae4b8a7c73387f
laska-adief <laskaadief.la@gmail.com>
2022-02-15 18:58:58 +0700

    create todo-complete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoCompleteComponent } from './todos/todo-complete/todo-complete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodosComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoCompleteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/todos/todo-complete/todo-complete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-complete',
+  templateUrl: './todo-complete.component.html',
+  styleUrls: ['./todo-complete.component.css']
+})
+export class TodoCompleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
