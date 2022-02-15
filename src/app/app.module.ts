649100978077420903148bc74664f380fb3780b9
laska-adief <laskaadief.la@gmail.com>
2022-02-15 18:58:25 +0700

    create todo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule
