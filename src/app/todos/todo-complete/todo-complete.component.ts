d961c66c56cc379735fa3724e570205d39a7ed64
laska-adief <laskaadief.la@gmail.com>
2022-02-15 19:43:41 +0700

    implement function get complete todo in todo-complete component

--- src/app/todos/todo-complete/todo-complete.component.ts
+++ src/app/todos/todo-complete/todo-complete.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
 
 @Component({
   selector: 'app-todo-complete',
   templateUrl: './todo-complete.component.html',
-  styleUrls: ['./todo-complete.component.css']
+  styleUrls: ['./todo-complete.component.css'],
 })
 export class TodoCompleteComponent implements OnInit {
-
-  constructor() { }
+  completeTodos: any = [];
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.getCompleteTodo();
+    console.log(this.completeTodos);
   }
 
+  getCompleteTodo() {
+    this.todoService.getTodos().subscribe((resp) => {
+      const completeTodoData = resp.filter(
+        (todo: any) => todo.isCompleted === true
+      );
+      this.completeTodos = completeTodoData;
+    });
+  }
 }
